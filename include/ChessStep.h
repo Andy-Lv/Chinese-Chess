3ec82f98aa45b0427a6682aa6cbb8dcee999d019
Andy-Lv <3304086606@qq.com>
2021-07-24 13:54:36 +0800

    MachineGame

---
+++ include/ChessStep.h
@@ -0,0 +1,25 @@
+//
+// Created by andy on 2021/7/24.
+//
+
+#ifndef CHINESECHESS_CHESSSTEP_H
+#define CHINESECHESS_CHESSSTEP_H
+
+#include <QObject>
+
+class ChessStep : public QObject
+{
+Q_OBJECT
+public:
+    explicit ChessStep(QObject *parent = 0);
+    ~ChessStep();
+
+    int m_nMoveID;   //移动棋子ID
+    int m_nKillID;   //将要被击杀的棋子ID
+    int m_nRowFrom;  //原位置的行
+    int m_nColFrom;  //原位置的列
+    int m_nRowTo;    //目的位置的行
+    int m_nnColTo;   //目的位置的列
+};
+
+#endif //CHINESECHESS_CHESSSTEP_H
---
+++ src/ChessStep.cpp
@@ -0,0 +1,13 @@
+//
+// Created by andy on 2021/7/24.
+//
+
+#include "ChessStep.h"
+
+ChessStep::ChessStep(QObject *parent) : QObject(parent)
+{
+}
+
+ChessStep::~ChessStep()
+{
+}
